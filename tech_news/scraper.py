229979c4ae30adafa849c77815977d014da82e02
Janaina Oliveira <janainapp@gmail.com>
2022-11-08 20:04:27 -0300

    fix: correct erros req1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,15 +1,22 @@
 import requests
+import time
+
 
 # Requisito 1
-def fetch(url: str, wait: int = 1) -> str:
+def fetch(url: str, wait: int = 3) -> str:
     """Seu código deve vir aqui"""
     try:
-        response = requests.get(url, timeout=wait)
+        time.sleep(1)
+        response = requests.get(
+            url,
+            headers={"user-agent": "Fake user-agent"},
+            timeout=wait
+        )
         response.raise_for_status()
-    except (requests.ReadTimeout):
+    except (requests.HTTPError, requests.ReadTimeout):
         return None
     else:
-         return response.text
+        return response.text
 
 
 # Requisito 2
